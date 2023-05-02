22d41be4af515d15e1c776bb156881d615203875
leventkarakus <leventbkarakus@hotmail.com>
2023-05-01 22:53:25 -0400

    skeleton of the function wit pseudo

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+/*
+If the values match, it should print (console.log) the following: Assertion 	Passed: [actual] === [expected] (but with the values filled in)
+
+Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
+
+*/
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
