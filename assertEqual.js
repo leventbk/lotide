423008945f9703cb18aca7b3a8cdd4eb8ed37ea5
leventkarakus <leventbkarakus@hotmail.com>
2023-05-02 09:50:38 -0400

    assert checking added, test file deleted

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,10 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-/*
-If the values match, it should print (console.log) the following: Assertion 	Passed: [actual] === [expected] (but with the values filled in)
-
-Otherwise it should print (console.log) the following: Assertion Failed: [actual] !== [expected] (but with the values filled in)
-
-*/
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
 // TEST CODE
