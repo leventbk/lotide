b1c1d9899a5c2b4107c033e68bea1d6780877b89
leventkarakus <leventbkarakus@hotmail.com>
2023-06-10 18:10:13 -0400

    2 new assert

---
+++ eqObjects.js
@@ -0,0 +1,28 @@
+const eqArrays = require('./eqArrays');
+
+const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+
+  if (object1Keys.length === object2Keys.length) {
+    for (const key of object1Keys) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (typeof object1[key] === 'object' && object1[key] !== null && typeof object2[key] === 'object' && object2[key] !== null) {
+        if (!eqObjects(object1[key], object2[key])) {
+          return false;
+        }
+      } else if (object1[key] !== object2[key]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+
+  return true;
+};
+
+module.exports = eqObjects;
